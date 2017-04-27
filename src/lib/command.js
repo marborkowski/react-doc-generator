1b976fc50b785f6d684efc9ba56ed4cf95461aed
frode carlsen <fc@steria.no>
2017-04-27 19:00:01 +0200

    regexp exclude pattern

--- src/lib/command.js
+++ src/lib/command.js
@@ -13,7 +13,7 @@ export default (function Command () {
     .usage(`<dir> [options]`)
     .option('-x, --extensions <items>', 'Include only these file extensions. Default: js,jsx', list, ['js', 'jsx'])
     .option('-i, --ignore <items>', 'Folders to ignore. Default: node_modules,__tests__,__mocks__', list, ['node_modules', '__tests__', '__mocks__'])
-    .option('-e, --exclude-patterns <items>', 'Filename patterns to exclude. Default: []', list, [])
+    .option('-e, --exclude-patterns <items>', 'Filename patterns to exclude. Default: []', list, ['.*.spec.jsx', '.*.spec.js' ])
 	.option('-f, --format <format>', 'Output format (markdown|asciiddoc) [asciidoc]')
     .option('-t, --title [value]', 'Document title. Default: \'Components\'', 'Components')
     .option('-o, --output <file>', 'Output file to write. Default (asciidoc): \'DOCUMENTATION.ADOC\'')
