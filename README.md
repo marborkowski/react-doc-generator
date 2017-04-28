de974a93aae11489cabf84c1953b8bb618c7f658
frode carlsen <fc@steria.no>
2017-04-28 07:29:22 +0200

    working exclude pattern using regexp

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # React DOC Generator
 [![npm version](https://img.shields.io/npm/v/react-asciidoc-generator.svg?style=flat-square)](https://www.npmjs.com/package/react-asciidoc-generator)
-[![dependency status](https://img.shields.io/david/frode-carlsen/react-asciidoc-generator.svg?style=flat-square)](https://david-dm.org/frode-carlsen/react-asciidoc-generator)
-[![build status](https://img.shields.io/travis/frode-carlsen/react-asciidoc-generator.svg?style=flat-square)](https://travis-ci.org/frode-carlsen/react-asciidoc-generator)
+[![dependency status](https://img.shields.io/david/zarbear/react-asciidoc-generator.svg?style=flat-square)](https://david-dm.org/zarbear/react-asciidoc-generator)
+[![build status](https://img.shields.io/travis/zarbear/react-asciidoc-generator.svg?style=flat-square)](https://travis-ci.org/zarbear/react-asciidoc-generator)
+
+This is a fork of [react-doc-generator](https://github.com/frode-carlsen/react-doc-generator) to provide additional features for asciidoc or custom templates.
 
 Generate simple React components documentation in either
-* [Asciidoc] (http://asciidoctor.org/docs/).
+* [Asciidoc](http://asciidoctor.org/docs/).
 * [Markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet).
 * Any custom handlebar template
 
@@ -28,7 +30,7 @@ Options:
   -x, --extensions <items>        Include only these file extensions. Default: js,jsx
   -f, --format <format>           Output format (asciiddoc|asciidoc) [asciidoc]
   -i, --ignore <items>            Folders to ignore. Default: node_modules,__tests__,__mocks__
-  -e, --exclude-patterns <items>  Filename patterns to exclude. Default: []
+  -e, --exclude-patterns <items>  Filename patterns to exclude (may be comma-separated list of regexp). Default: '*.spec.jsx','*.spec.js' 
   -t, --title [value]>            Document title. Default: 'Components'
   -o, --output <file>             Output file to write. Default (asciidoc): 'DOCUMENTATION.ADOC'. Default (markdown): 'DOCUMENTATION.MD'
   --handlebar-template [path]     Custom handlebar output template. Alternative to --format.
@@ -116,27 +118,6 @@ This is an example of what you'll see in your terminal.
 Forked from original module 'react-doc-generator' by Marcin Borowski (https://github.com/marborkowski/react-doc-generator)
 
 ### Licenses
-
-<sub>MIT License</sub>  
-<sub>Copyright (c) 2017 Frode Carlsen</sub>  
-<sub>Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:</sub>
-
-<sub>The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.</sub>
-
-<sub>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.</sub>
-
 
 <sub>MIT License</sub>  
 <sub>Copyright (c) 2017 Marcin Borkowski (<marborowski@gmail.com>)</sub>  
--- src/react-asciidoc-generator.js
+++ src/react-asciidoc-generator.js
@@ -63,65 +63,67 @@ if (Command.args.length !== 1) {
 	const output = fs.createWriteStream(outputConfig.outputFile);
 	const template = Handlebars.compile(`${fs.readFileSync(outputConfig.template)}`);
 
+	const excludeFilePatterns = new RegExp('(?:' + Command.excludePatterns.join('|') + ')$');
     readFiles(
         Command.args[0],
         {
             match: new RegExp('\\.(?:' + Command.extensions.join('|') + ')$'),
-            exclude: Command.excludePatterns,
             excludeDir: Command.ignore,
         },
         (err, content, filename, next) => {
             if (err) {
                 throw err;
             }
+			
+			if(!filename.match(excludeFilePatterns)) {
 
-            try {
-                let components = parse(content, resolver.findAllExportedComponentDefinitions);
-                components = components.map(component => {
-                    if (component.description && !component.displayName) {
-                        component.title = component.description.match(/^(.*)$/m)[0];
-                        if (component.description.split('\n').length > 1) {
-                            component.description = component.description.replace(/[\w\W]+?\n+?/, '');
-                            component.description = component.description.replace(/(\n)/gm, '   \n');
-                        } else {
-                            component.description = null;
-                        }
-                    } else {
-                        component.title = component.displayName;
-                    }
+				try {
+					let components = parse(content, resolver.findAllExportedComponentDefinitions);
+					components = components.map(component => {
+						if (component.description && !component.displayName) {
+							component.title = component.description.match(/^(.*)$/m)[0];
+							if (component.description.split('\n').length > 1) {
+								component.description = component.description.replace(/[\w\W]+?\n+?/, '');
+								component.description = component.description.replace(/(\n)/gm, '   \n');
+							} else {
+								component.description = null;
+							}
+						} else {
+							component.title = component.displayName;
+						}
 
-                    if (component.description) {
-                        component.description = `${component.description}   \n\n`;
-                    }
+						if (component.description) {
+							component.description = `${component.description}   \n\n`;
+						}
 
-                    // validate default values
-                    if (component.props) {
-                        Object.keys(component.props).forEach(key => {
-                            let obj = component.props[key];
-                            if (obj.defaultValue) {
-                                if ((/[^\w\s.&:\-+*,!@%$]+/igm).test(obj.defaultValue.value)) {
-                                    obj.defaultValue.value = '<See the source code>';
-                                }
-                            }
-                        });
-                    }
+						// validate default values
+						if (component.props) {
+							Object.keys(component.props).forEach(key => {
+								let obj = component.props[key];
+								if (obj.defaultValue) {
+									if ((/[^\w\s.&:\-+*,!@%$]+/igm).test(obj.defaultValue.value)) {
+										obj.defaultValue.value = '<See the source code>';
+									}
+								}
+							});
+						}
 
-                    return component;
-                });
-                templateData.files.push({ filename, components });
-                table.push([
-                    filename,
-                    components.length,
-                    Colors.green(`OK.`)
-                ]);
-            } catch (e) {
-                table.push([
-                    filename,
-                    0,
-                    Colors.red(`You have to export at least one valid React Class!`)
-                ]);
-            }
-
+						return component;
+					});
+					templateData.files.push({ filename, components });
+					table.push([
+						filename,
+						components.length,
+						Colors.green(`OK.`)
+					]);
+				} catch (e) {
+					table.push([
+						filename,
+						0,
+						Colors.red(`You have to export at least one valid React Class!`)
+					]);
+				}
+			}
             next();
         },
         err => {
