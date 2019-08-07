f75925290851d44c6380024423e0e2f0a90a79dc
Rahul Ravindran <rravindran@metocean.com>
2019-08-07 13:00:19 -0300

    Working tests

--- src/__tests__/react-doc-generator.test.js
+++ src/__tests__/react-doc-generator.test.js
@@ -1,134 +1,112 @@
 // https://travis-ci.org/marborkowski/react-doc-generator
 
+require("@babel/polyfill");
 const TEST_TIMEOUT = 120000;
 jasmine.DEFAULT_TIMEOUT_INTERVAL = TEST_TIMEOUT; // eslint-disable-line no-undef
 
-const path = require('path');
-const fs = require('fs');
-const spawn = require('child_process').spawn;
-const pkg = require('../../package.json');
+const path = require("path");
+const fs = require("fs");
+const spawn = require("child_process").spawn;
+const pkg = require("../../package.json");
 
-function run(args) {
-
+function run(command, args) {
   let stdout = [];
   let stderr = [];
 
-  return new Promise((resolve,reject) => {
-      let binPath = path.join(__dirname, '../../dist/react-doc-generator.js')
-      fs.chmodSync(binPath, '0777');
-      console.log(binPath,args)
-      let spawned = spawn(binPath, args);
-
-      spawned.stdout.on('data', data => stdout.push(data));
-      spawned.stderr.on('data', data => stderr.push(data));
-      spawned.on('close', () => resolve([stdout.join(''), stderr.join('')]));
-      spawned.on('error', err => { throw err; } );
-  }).catch((error) => {
-    console.log(error,'Error')
-    throw error
-  })
+  return new Promise((resolve, reject) => {
+    let spawned = spawn(command, args);
+    spawned.stdout.on("data", data => {
+      stdout.push(data);
+    });
+    spawned.stderr.on("data", data => stderr.push(data));
+    spawned.on("close", () => resolve([stdout.join(""), stderr.join("")]));
+    spawned.on("error", err => {
+      throw err;
+    });
+  }).catch(error => {
+    console.log(error, "Error");
+    throw error;
+  });
 }
 
 function loadDoc() {
   return new Promise((resolve, reject) => {
-      fs.readFile('./dist/DOCUMENTATION.md', 'utf8', function(err, data) {
-          if (err) {
-              reject(err);
-          } else {
-              resolve(data);
-          }
-      });
-  })
-}
-
-describe('react-doc-generator', () => {
-    it('has the proper console output', () => {
-        return run(
-            [
-              'src/__mocks__',
-              '-o',
-              './dist/DOCUMENTATION.md'
-            ]
-        ).then((stdout, stderr) => {
-            if (stderr) {
-                throw stderr;
-            }
-
-            const output = stdout[0];
-            const lines = output.split('\n').filter(line => line.length > 0);
-            expect(lines[0]).toContain(pkg.version);
-            expect(lines[1]).toContain('Marcin Borkowski <marborkowski@gmail.com>');
-        });
-    }, TEST_TIMEOUT);
-
-    it('return the proper message when given extensions not found', () => {
-        return run(
-            [
-              'src/__mocks__',
-              '-o',
-              './dist/DOCUMENTATION.md',
-              '-x',
-              '4hs0,kku4'
-            ]
-        ).then((stdout, stderr) => {
-            if (stderr) {
-                throw stderr;
-            }
-
-            const output = stdout[0];
-            const lines = output.split('\n').filter(line => line.length > 0);
-            expect(lines[2]).toContain('Warning');
-            expect(lines[2]).toContain('*.4hs0');
-            expect(lines[2]).toContain('*.kku4');
-        });
-    }, TEST_TIMEOUT);
-
-    it('contains help section if no argument is available in query', () => {
-        return run(['-o', './dist/DOCUMENTATION.md']).then((stdout, stderr) => {
-            if (stderr) {
-                throw stderr;
-            }
-
-            const output = stdout[0];
-            expect(output).toContain('Usage:');
-            expect(output).toContain('Options:');
-        });
-    }, TEST_TIMEOUT);
-
-    describe('output file', () => {
-        it('has needed values', () => {
-            return run(
-                [
-                  'src/__mocks__',
-                  '-o',
-                  './dist/DOCUMENTATION.md',
-                  '-t',
-                  'MyTitleXYZ'
-                ]
-            ).then((stdout, stderr) => {
-                if (stderr) {
-                    throw stderr;
-                }
-
-                return loadDoc().then(
-                    result => {
-                        const lines = result.split('\n');
-                        expect(lines[0]).toContain('MyTitleXYZ');
-                        expect(result).toContain('Property | Type | Required | Default value | Description');
-                        expect(result).toContain(':--- | :--- | :--- | :--- | :---');
-                        expect(result).toContain('Description of prop &quot;toe&quot; has one break line here following more comments and has default empty string.');
-                        expect(result).toContain('toe|string|no|&#x27;&#x27;|');
-                        expect(result).toContain('finger|string|no|&#x27;medium&#x27;|');
-                        expect((/^\*\*([a-zA-Z/_.]+)\*\*$/igm).test(result)).toBeTruthy();
-                        expect((/^### (.*)$/igm).test(result)).toBeTruthy();
-                        expect(lines[lines.length - 2]).toContain(pkg.version);
-                        expect(lines[lines.length - 2]).toContain('marborkowski');
-                    },
-                    reason => {
-                        throw reason;
-                    }
-                );
-            });
-        }, TEST_TIMEOUT);
+    fs.readFile("./dist/DOCUMENTATION.md", "utf8", function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
     });
+  });
+}
+let binPath = path.join(__dirname, "../../dist/react-doc-generator.js");
+  fs.chmodSync(binPath, "0777");
+describe("react-doc-generator", () => {
+  
+  it("has the proper console output", async () => {
+    try {
+      const stdout = await run("node", [
+        binPath,
+        "src/__mocks__",
+        "-o",
+        "./dist/DOCUMENTATION.md"
+      ]);
+      const output = stdout[0];
+      expect(output).toMatchSnapshot()
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  });
+
+  it("return the proper message when given extensions not found", async () => {
+    try {
+      const stdout = await run("node", [
+        binPath,
+        "src/__mocks__",
+        "-o",
+        "./dist/DOCUMENTATION.md",
+        "-x",
+        "4hs0,kku4"
+      ]);
+      const output = stdout[0];
+      expect(output).toMatchSnapshot()
+    } catch (e) {
+      throw e;
+    }
+  });
+
+  it("contains help section if no argument is available in query", async () => {
+    try {
+      const stdout = await run("node", [
+        binPath,
+        "-o",
+        "./dist/DOCUMENTATION.md"
+      ]);
+      const output = stdout[0];
+      expect(output).toMatchSnapshot()
+    } catch (e) {
+      throw e;
+    }
+  });
+});
+describe("output file", () => {
+  it("has needed values", async () => {
+    try {
+      await run("node",[
+        binPath,
+        "src/__mocks__",
+        "-o",
+        "./dist/DOCUMENTATION.md",
+        "-t",
+        "MyTitleXYZ"
+      ]);
+      const result = await loadDoc();
+      const lines = result.split("\n");
+      expect(lines).toMatchSnapshot()
+    } catch (e) {
+      throw e;
+    }
+  });
 });
--- src/react-doc-generator.js
+++ src/react-doc-generator.js
@@ -1,39 +1,52 @@
 #!/usr/bin/env node
 require("@babel/polyfill");
-import fs from 'fs'
-import generateReactDocs from './generatereactdoc'
-import Command from './lib/command.js';
-import Handlebars from 'handlebars';
+import fs from "fs";
+import path from "path";
+import generateReactDocs from "./generatereactdoc";
+import Command from "./lib/command.js";
+import Handlebars from "handlebars";
 
-import Colors from 'colors';
-import Table from 'cli-table';
-Handlebars.registerHelper('inc', (value, options) => {
-    return parseInt(value, 10) + 1;
+import Colors from "colors";
+import Table from "cli-table";
+Handlebars.registerHelper("inc", (value, options) => {
+  return parseInt(value, 10) + 1;
 });
 (async () => {
-    const pkg = require('../package.json');
-    const template = Handlebars.compile(`${fs.readFileSync(path.join(__dirname, 'template.handlebars'))}`);
-    const table = new Table({
-        head: [
-            Colors.cyan('Path'),
-            Colors.cyan('Components'),
-            Colors.cyan('Status')
-        ]
-    });
-    console.log(Colors.white(`\n\nREACT DOC GENERATOR v${pkg.version}`));
-    console.log(Colors.white(`by Marcin Borkowski <marborkowski@gmail.com>`));
-    try{
-        if (Command.args.length !== 1) {
-            console.log(`${Colors.red('Please specify <dir> as the first argument!')}`);
-            Command.help();
-        } else {
-            const templateData = await generateReactDocs(Command.args[0],Command.extensions,Command.excludePatterns,Command.ignore,Command.output )
-            const outputFile = fs.createWriteStream(Command.output);
-            outputFile.write(template(templateData))
-        }
-    }catch(e){
-        console.error('Error occurred',e)
+  const pkg = require("../package.json");
+  const template = Handlebars.compile(
+    `${fs.readFileSync(path.join(__dirname, "template.handlebars"))}`
+  );
+  const table = new Table({
+    head: [
+      Colors.cyan("Path"),
+      Colors.cyan("Components"),
+      Colors.cyan("Status")
+    ]
+  });
+  console.log(Colors.white(`\n\nREACT DOC GENERATOR v${pkg.version}`));
+  console.log(Colors.white(`by Marcin Borkowski <marborkowski@gmail.com>`));
+  try {
+    if (Command.args.length !== 1) {
+      console.log(
+        `${Colors.red("Please specify <dir> as the first argument!")}`
+      );
+      Command.help();
+    } else {
+      const [templateData,cliOutput] = await generateReactDocs({
+        sourceDir: Command.args[0],
+        extensions: Command.extensions,
+        excludePatterns: Command.excludePatterns,
+        ignoreDirectory: Command.ignore,
+        outputDir: Command.output
+      });
+      const outputFile = fs.createWriteStream(Command.output);
+      outputFile.write(template(templateData));
+      cliOutput.forEach((cliRow) => {
+          table.push(cliRow)
+      })
+      console.log(table.toString())
     }
-   
-})()
-
+  } catch (e) {
+    console.error("Error occurred", e);
+  }
+})();
